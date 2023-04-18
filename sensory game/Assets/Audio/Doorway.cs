5104b864e1f022e883fffafa4c62040567f277cf
StarManEXE <114677798+StarManEXE@users.noreply.github.com>
2023-04-18 05:49:50 -0400

    Final Trippy

--- sensory game/Assets/Audio/Doorway.cs
+++ sensory game/Assets/Audio/Doorway.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Doorway : MonoBehaviour
 {
+
+    public GameObject objectToDisable;
+    public GameObject objectToEnable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            objectToDisable.SetActive(false);
+            objectToEnable.SetActive(true);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectToDisable.SetActive(true);
+        objectToEnable.SetActive(false);
     }
 
     // Update is called once per frame
